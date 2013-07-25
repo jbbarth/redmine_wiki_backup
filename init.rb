622028637d9d3c5ab96b582edc6b5232b5943141
jbbarth <jeanbaptiste.barth@gmail.com>
2013-07-25 18:28:06 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_wiki_backup do
+  name 'Redmine Wiki Backup plugin'
+  author 'Jean-Baptiste BARTH'
+  description 'Backup your wiki pages as a statci website'
+  version '0.0.1'
+  url 'https://github.com/jbbarth/redmine_wiki_backup'
+  author_url 'jeanbaptiste.barth@gmail.com'
+end
