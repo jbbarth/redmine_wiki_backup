eb11ee616a8eaf4ae0b9572f87c4e3ab407fe40c
jbbarth <jeanbaptiste.barth@gmail.com>
2013-10-02 19:00:09 +0200

    Add some comments in example script

--- extra/mirror.sh
+++ extra/mirror.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+#put your redmine URL here, without a trailing slash!
 host=http://localhost:3000
+#the API token of the user that will perform the download
 token=13d57affff4adfc03aa815af9ec6b7d608fd7045
-#without trailing slash!!!
+#the directory you can to backup to, without trailing slash!
 dir=/tmp/wiki_backup
 
+#cleanup the directory
 rm -rf $dir
 
+#mirror the website
 wget --mirror --timestamping --no-verbose --page-requisites --base=./ --adjust-extension \
      --directory-prefix $dir --no-host-directories --cut-dirs=1 --convert-links --no-parent \
      --reject "*?parent=*" --header="X-Redmine-Api-Key: $token" \
