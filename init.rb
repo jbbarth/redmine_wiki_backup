ad29bb72c74816f0d0b9e8a379c24d7435b7de19
Vincent Robert <vincent.robert@nanego.com>
2015-03-19 10:20:40 +0100

    Fix typo in plugin description

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 Redmine::Plugin.register :redmine_wiki_backup do
   name 'Redmine Wiki Backup plugin'
   author 'Jean-Baptiste BARTH'
-  description 'Backup your wiki pages as a statci website'
+  description 'Backup your wiki pages as a static website'
   version '0.0.1'
   url 'https://github.com/jbbarth/redmine_wiki_backup'
   author_url 'jeanbaptiste.barth@gmail.com'
