bfd7bcae31be58c7ea4e5afbed8f88ae3c6914a9
Vincent Robert <vincent.robert@nanego.com>
2023-03-16 14:55:30 +0100

    Avoid N+1 queries: preload projects wikis

--- app/helpers/wiki_backup_helper.rb
+++ app/helpers/wiki_backup_helper.rb
@@ -2,9 +2,10 @@ module WikiBackupHelper
   def projects
     projects_with_existing_wiki = Wiki.includes(:project)
                                       .where(:id => WikiPage.pluck(:wiki_id))
-                                      .pluck(:project_id) 
+                                      .pluck(:project_id)
     @projects ||= Project.visible
                          .active
+                         .includes(:wiki)
                          .has_module(:wiki)
                          .where(:id => projects_with_existing_wiki)
   end
@@ -13,17 +14,17 @@ module WikiBackupHelper
     ary = []
     Project.project_tree(projects) do |project, level|
       title = "#{level == 0 ? "" : "--" * level + " " }#{project.name}"
-      ary << [title, project.identifier, {"data-link-id" => "wiki_project_#{project.identifier}", "data-start-page" => project.wiki.start_page}]
+      ary << [title, project.identifier, { "data-link-id" => "wiki_project_#{project.identifier}", "data-start-page" => project.wiki.start_page }]
     end
     ary
   end
 
-  def project_pages_options(pages, node=nil, level = 0)
+  def project_pages_options(pages, node = nil, level = 0)
     ary = []
     if pages[node]
       pages[node].each do |page|
         title = "#{level == 0 ? "" : "--" * level + " "}#{h(page.pretty_title)}"
-        ary << [title, page.title, {"data-link-id" => "wiki_page_#{page.id}"}]
+        ary << [title, page.title, { "data-link-id" => "wiki_page_#{page.id}" }]
         ary += project_pages_options(pages, page.id, level + 1) if pages[page.id]
       end
     end
@@ -31,18 +32,18 @@ module WikiBackupHelper
   end
 
   def other_formats_links(&block)
-    #nothing for wiki backups!
+    # nothing for wiki backups!
   end
 
   def url_for(options = {})
     if options.is_a?(Hash)
-      #patch links to wiki pages
+      # patch links to wiki pages
       if options[:controller] == 'wiki' && options[:context] != 'top_menu' && options[:action] != 'new'
         options[:controller] = 'wiki_backup'
       end
-      #patch links to attachments
-      #it just adds an option so that the route that will be picked will be the one with the '/wiki_backup'
-      #prefix, so wget --mirror actually mirror those files too.
+      # patch links to attachments
+      # it just adds an option so that the route that will be picked will be the one with the '/wiki_backup'
+      # prefix, so wget --mirror actually mirror those files too.
       if options[:controller] == 'attachments' && options[:action].in?(%w(download show thumbnail))
         options[:context] = 'wiki_backup'
         # add filename automatically so that we don't have both attachments/1234 AND attachments/1234/name.jpg,
